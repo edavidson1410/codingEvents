997695b8f025c9809c9a640cfefba621099ad861
Eric Davidson <edavidson1410@gmail.com>
2023-10-05 17:16:22 -0500

    Enum chapter done.

--- src/main/java/org/launchcode/codingEvents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingEvents/controllers/EventController.java
@@ -3,6 +3,7 @@ package org.launchcode.codingEvents.controllers;
 import jakarta.validation.Valid;
 import org.launchcode.codingEvents.data.EventData;
 import org.launchcode.codingEvents.models.Event;
+import org.launchcode.codingEvents.models.EventType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -29,11 +30,12 @@ public class EventController {
     public String renderCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
         model.addAttribute(new Event());
+        model.addAttribute("types", EventType.values());
         return "events/create";
     }
 
     //model binding
-    @PostMapping
+    @PostMapping("create")
     public String createEvent(@ModelAttribute @Valid Event newEvent,
                               Errors errors, Model model) {
         if(errors.hasErrors()) {
